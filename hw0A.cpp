77d1027ecc791bbdd175dad142d1e3ea1d5870fe
owen-keating <127237877+owen-keating@users.noreply.github.com>
2023-03-07 19:48:01 +0000

    Add files via upload

---
+++ hw0A.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  hw0A
+//
+//  Created by Hello on 2/4/21.
+//
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main(){
+    
+    double theta1, theta2, n1, n2;
+    cin >> theta1;
+    cin >> n1;
+    cin >> n2;
+    theta2 = asin(sin(theta1 * M_PI / 180) * (n1/n2)) * 180 / M_PI;
+    cout << theta2;
+    
+}
